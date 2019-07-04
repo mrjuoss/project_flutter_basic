12cdf85dfd9d7421d710e741896f7c26f59320ef
mrjuoss <mrjuoss@gmail.com>
2019-07-04 21:41:32 +0700

    Final Project

--- lib/pages/detail_card.dart
+++ lib/pages/detail_card.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
-import 'model_user.dart';
+import 'package:tugas_akhir_osg04028/models/my_card.dart';
 
-class DetailUser extends StatelessWidget {
-  final User user;
-  DetailUser(this.user);
+class DetailCard extends StatelessWidget {
+  final MyCard myCard;
+  DetailCard(this.myCard);
 
   @override
   Widget build(BuildContext context) {
     final topContentText = Container(
         margin: EdgeInsets.fromLTRB(30.0, 250.0, 0.0, 0.0),
-        child: Text(user.name,
+        child: Text(myCard.name,
             style: TextStyle(fontSize: 20.0, color: Colors.white)));
 
     final topContent = Stack(
@@ -19,7 +19,7 @@ class DetailUser extends StatelessWidget {
             height: MediaQuery.of(context).size.height * 0.5,
             decoration: new BoxDecoration(
               image: new DecorationImage(
-                image: NetworkImage(user.picture),
+                image: NetworkImage(myCard.cardImages[0].imageUrl),
                 fit: BoxFit.cover,
               ),
             )),
@@ -27,7 +27,7 @@ class DetailUser extends StatelessWidget {
           height: MediaQuery.of(context).size.height * 0.5,
           padding: EdgeInsets.all(10.0),
           width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(color: Color.fromRGBO(58, 66, 86, .9)),
+          decoration: BoxDecoration(color: Color.fromRGBO(58, 66, 86, .5)),
           child: topContentText,
         ),
         Positioned(
@@ -44,8 +44,8 @@ class DetailUser extends StatelessWidget {
     );
 
     final bottomContentText = Text(
-      user.about,
-      style: TextStyle(fontSize: 12.0),
+      myCard.desc,
+      style: TextStyle(fontSize: 14.0),
     );
 
     final bottomContent = Container(
---
+++ lib/pages/home.dart
@@ -0,0 +1,195 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:tugas_akhir_osg04028/models/my_card.dart';
+import 'package:tugas_akhir_osg04028/pages/detail_card.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Final Project - OSG04028'),
+      ),
+      body: Container(
+        child: FutureBuilder(
+          future: getData(http.Client()),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.data == null) {
+              return Container(
+                child: Center(
+                  child: Text('Loading...'),
+                ),
+              );
+            } else {
+              return ListView.builder(
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                itemCount: snapshot.data.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    elevation: 8.0,
+                    margin:
+                        EdgeInsets.symmetric(horizontal: 10.0, vertical: 3.0),
+                    child: Container(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 10.0, vertical: 10.0),
+                      decoration:
+                          //BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
+                          BoxDecoration(color: Color.fromRGBO(178, 89, 26, .9)),
+                      child: ListTile(
+                        contentPadding: EdgeInsets.symmetric(
+                            horizontal: 10.0, vertical: 5.0),
+                        leading: CircleAvatar(
+                          backgroundImage: NetworkImage(
+                              snapshot.data[index].cardImages[0].imageUrl),
+                        ),
+                        title: Text(
+                          snapshot.data[index].name,
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                        ),
+                        subtitle: Text(
+                          snapshot.data[index].type,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        trailing: Icon(
+                          Icons.keyboard_arrow_right,
+                          color: Colors.white,
+                          size: 30.0,
+                        ),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            new MaterialPageRoute(
+                                builder: (context) =>
+                                    DetailCard(snapshot.data[index])),
+                          );
+                        },
+                      ),
+                    ),
+                  );
+                },
+              );
+            }
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: Text('Confirmation'),
+                  content: Text('Are you sure want to Exit ?'),
+                  actions: <Widget>[
+                    FlatButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text('No'),
+                    ),
+                    FlatButton(
+                      onPressed: () {
+                        exit(0);
+                      },
+                      child: Text('Yes'),
+                    ),
+                  ],
+                );
+              });
+        },
+        child: Icon(Icons.exit_to_app),
+      ),
+    );
+  }
+}
+
+List<MyCard> parseCard(String responBody) {
+  final parsed = json.decode(responBody).cast<Map<String, dynamic>>();
+  return parsed.map<MyCard>((json) => MyCard.fromJson(json)).toList();
+}
+
+Future<List<MyCard>> getData(http.Client client) async {
+  final response =
+      await client.get('https://db.ygoprodeck.com/api/v5/cardinfo.php?num=20');
+
+  return compute(parseCard, response.body);
+}
+
+class CardList extends StatelessWidget {
+  final List<MyCard> cards;
+
+  CardList({Key key, this.cards}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: FutureBuilder(
+        future: getData(http.Client()),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.data == null) {
+            return Container(
+              child: Center(
+                child: Text('Loading...'),
+              ),
+            );
+          } else {
+            return ListView.builder(
+              scrollDirection: Axis.vertical,
+              shrinkWrap: true,
+              itemCount: snapshot.data.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Card(
+                  elevation: 8.0,
+                  margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 3.0),
+                  child: Container(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                    decoration:
+                        BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
+                    child: ListTile(
+                      contentPadding:
+                          EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
+                      leading: CircleAvatar(
+                        backgroundImage:
+                            NetworkImage(snapshot.data[index].picture),
+                      ),
+                      title: Text(
+                        snapshot.data[index].name,
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: Text(
+                        snapshot.data[index].type,
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      trailing: Icon(
+                        Icons.keyboard_arrow_right,
+                        color: Colors.white,
+                        size: 30.0,
+                      ),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          new MaterialPageRoute(
+                              builder: (context) =>
+                                  DetailCard(snapshot.data[index])),
+                        );
+                      },
+                    ),
+                  ),
+                );
+              },
+            );
+          }
+        },
+      ),
+    );
+  }
+}
